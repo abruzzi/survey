347c3bc3db0188a3df8be038e615fbd61752726e
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-22 20:46:24 +1100

    Refactor to dynamic generating

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,47 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const App = () => {
-  const qna = {
+const survey = [
+  {
+    id: 'q1',
+    type: 'text',
+    question: 'What is your name?',
+  },
+  {
+    id: 'q2',
+    type: 'text',
+    question: 'What is your address?',
+  },
+  {
+    id: 'q3',
+    type: 'radio',
     question: 'How are you?',
     answers: [
       { label: 'I am great', value: 'great'},
       { label: 'Not too bad', value: 'not-bad'}
     ]
+  }
+];
+
+const InputField = ({field}) => {
+  const inputFieldMap = {
+    text: (question) => (<TextField label={question.question} placeholder={question.question} />),
+    radio: (question) => (<QuestionAndAnswers question={question.question} answers={question.answers} onChange={() => {}}/>)
   };
 
+  return inputFieldMap[field.type](field);
+};
+
+const App = () => {
   return (<Container>
     <Row>
-      <Col span={24}>
-        <TextField label="Name" placeholder="Type your name here"/>
-      </Col>
-
-      <Col span={24}>
-        <TextField label="Address" placeholder="And you live in"/>
-      </Col>
-
-      <Col span={24}>
-        <QuestionAndAnswers
-          question={qna.question}
-          answers={qna.answers}
-          onChange={() => {}}
-        />
-      </Col>
+      {
+        survey.map((s, index) => (
+          <Col span={24} key={index}>
+            <InputField field={s} />
+          </Col>
+        ))
+      }
     </Row>
   </Container>);
 };
