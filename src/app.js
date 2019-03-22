23b8d7356009a3dbf9c823451b0458de0bedf32b
Juntao Qiu <juntao.qiu@gmail.com>
2019-03-22 18:38:36 +1100

    introduce with react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('hello qna');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello Q and A</h1>
+  </div>
+);
+
+const app = document.querySelector('#app');
+
+ReactDOM.render(<App/>, app);
